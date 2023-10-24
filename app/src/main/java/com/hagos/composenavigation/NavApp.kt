ddb0617f4497cef67a882a4b05e72db096590801
Hagos Alema <hagosalema.mit@gmail.com>
2023-10-24 18:24:16 +0900

    initial commit

---
+++ app/src/main/java/com/hagos/composenavigation/NavApp.kt
@@ -0,0 +1,8 @@
+package com.hagos.composenavigation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NavApp: Application() {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Compose Navigation"
+include(":app")
+include(":core:data")
+include(":core:ui")
+include(":core:designsystem")
+include(":core:common")
+include(":core:model")
+include(":core:database")
+include(":core:network")
+include(":core:datastore")
+include(":domain")
